b4bb03af488e8595f85a1a4c2de773a00a6c2c1b
rrskami <65990432+rrskami@users.noreply.github.com>
2023-03-13 22:11:28 -0300

    primeiro commit das funcionalidades da dex com a poke api

--- js/script.js
+++ js/script.js
@@ -1,10 +1,78 @@
 
-async function listPoke (){
-    try {
-        const pokeData = await instance.get('pokemon/ditto');
-        console.log(pokeData);
+const divConteiner = document.querySelector('.display-show');
+const btnPrev = document.getElementById('prev');
+const btnNext = document.getElementById('next');
+const input = document.getElementById('input');
+const btnReset = document.getElementById('btn-top-left');
+const btnSearch = document.getElementById('btn-top-right');
+
+let pokeId = 1;
+listPoke(pokeId);
+
+btnPrev.addEventListener('click', (event)=>{
+    event.stopPropagation();
+    event.preventDefault();
+    pokeId -=1;
+    divConteiner.innerHTML="";
+    listPoke(pokeId);
+});
+
+btnNext.addEventListener('click', (event)=>{
+    event.stopPropagation();
+    event.preventDefault();
+    pokeId += 1;
+    divConteiner.innerHTML="";
+    listPoke(pokeId)
+});
+
+btnReset.addEventListener('click', (event)=>{
+    event.stopPropagation();
+    event.preventDefault();
+    input.value="";
+    divConteiner.innerHTML="";
+    listPoke(1);
+    console.log(pokeId)
+})
+
+btnSearch.addEventListener('click', (event)=>{
+    event.stopPropagation();
+    event.preventDefault();
+    pokeId = input.value;
+    divConteiner.innerHTML="";
+    listPoke(pokeId);
+    input.value = "";
+})
+
+input.addEventListener('keyup', (event)=>{
+    event.stopPropagation();
+    event.preventDefault();
+})
+
+async function listPoke (pokemonPosition){
+    try {       
+        const pokeData = await instance.get(`pokemon/${pokemonPosition}`);
+        //console.log(pokeData.data.abilities[0].ability.name);
+        pokeId=pokeData.data.id;
+        console.log(pokeId);
+        pokemonRender(pokeData);
     } catch (error) {
         console.log(error);
     }
 }
-listPoke();
+
+function pokemonRender (pokeData){
+    divConteiner.innerHTML +=
+    `
+        <div id='side-left-data'>
+            <h1 id='poke-name'>${pokeData.data.name}</h1>
+            <h2 id='poke-id'>#${pokeData.data.id}</h2>
+            <p> Type: ${pokeData.data.types.map(typeElement=>typeElement.type.name).join(" | ")}</p>
+            <p> Ability: ${pokeData.data.abilities[0].ability.name}</p>
+          
+            <p>height: ${pokeData.data.height/10}m | weigth: ${pokeData.data.weight/10}kg</p>
+        </div>
+        <div id='side-right-data'>
+            <img id='picture-card' src='${pokeData.data.sprites.front_default}'>    
+        </div>
+    `
+}
